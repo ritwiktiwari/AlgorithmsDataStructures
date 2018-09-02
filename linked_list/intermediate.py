54cdd7cfcf3d6cabc1ffa35cfeda60ea71e92949
Ritwik <ritwiktiwari24@gmail.com>
2018-09-02 23:40:23 +0530

    singly linked list implemented

--- linked_list/intermediate.py
+++ linked_list/intermediate.py
@@ -30,15 +30,57 @@ class LinkedList(object):
                 current = current.nextNode
             current.nextNode = newNode
             self.counter+=1
-        
 
+    def insert_beg(self,data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.counter+=1
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+            self.counter+=1
+
+    def remove_beg(self):
+        if self.head is None:
+            print("Linked List is already empty")
+        else:
+            currentNode = self.head
+            self.head = self.head.nextNode
+            self.counter-=1
+            del currentNode
+
+    def remove_end(self):
+        if self.head is None:
+            print("Linked List is already empty")
+        else:
+            current_head = self.head
+            while current_head.nextNode is not None:
+                current_head = current_head.nextNode
+            current_node = current_head
+            current_head.nextNode = None
+            del current_node.nextNode
+            self.counter-=1    
+        
+ 
 linkedlist = LinkedList()
+linkedlist.remove_beg()
 linkedlist.insert_end(1)
 linkedlist.insert_end(2)
 linkedlist.insert_end(3)
 linkedlist.insert_end(4)
 print("printing list")
 linkedlist.print_list()
+linkedlist.insert_beg(22)
+print("printing list after addition of 22 in the beginning")
+linkedlist.print_list()
+linkedlist.remove_beg()
+print("printing list")
+linkedlist.print_list()
 print("printing size")
 linkedlist.size()
+linkedlist.insert_end(55)
+linkedlist.print_list()
+linkedlist.remove_end()
+linkedlist.print_list()
 
