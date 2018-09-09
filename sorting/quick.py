7317bf76678b1c9571a8ff8018b2df3e37c6ef86
Ritwik <ritwiktiwari24@gmail.com>
2018-09-09 20:04:58 +0530

    implemented quicksort

---
+++ sorting/quick.py
@@ -0,0 +1,23 @@
+def quicksort(arr,low,high):
+    if low>=high:
+        return
+    else:
+        pivot = partition(arr,low,high)
+        quicksort(arr,low,pivot-1)
+        quicksort(arr,pivot+1,high)
+
+def partition(arr,low,high):
+    pivot = (low+high)//2
+    arr[pivot],arr[high] = arr[high],arr[pivot]
+    i = low
+    for j in range(low,high):
+        if arr[j]<=arr[high]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i+=1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
+
+# array = [23, 6, 4, -1, 0, 12, 8, 3, 1]
+array = [15, 8, 5, 9, 12, 6, 7, 20]
+quicksort(array,0,len(array)-1)
+print(array)
