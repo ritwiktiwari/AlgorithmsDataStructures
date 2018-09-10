762041651222dcef194a70f0ad9da9789f6de29d
Ritwik <ritwiktiwari24@gmail.com>
2018-09-10 22:46:44 +0530

    array rotation one by one

--- arrays/rotation.py
+++ arrays/rotation.py
@@ -1,9 +1,11 @@
 def rotate(arr,r):
-    for i in range(r):
+    i=0
+    while i<r:
         temp = arr[0]
         for j in range(len(arr)-1):
             arr[j]=arr[j+1]
         arr[-1]=temp
+        i+=1
     return arr
 
 arr = [1,2,3,4,5]
