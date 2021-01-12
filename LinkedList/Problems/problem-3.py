638ac8c67cf8cb2cf220ac73b87ecf833c7d668e
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2021-01-12 09:33:08 +0530

    cycle detection with hash table

--- LinkedList/Problems/problem-3.py
+++ LinkedList/Problems/problem-3.py
@@ -14,15 +14,28 @@ b = Node(2, c)
 a = Node(1, b)
 h.next_node = c
 
-head = a
 
-tortoise = hare = head
+def tortoise_hare(head):
+    tortoise = hare = head
+    while hare is not None:
+        tortoise = tortoise.next_node
+        hare = hare.next_node.next_node
+        if hare is tortoise:
+            print(f"Cycle found at {tortoise.data}")
+            break
 
-while hare is not None:
-    tortoise = tortoise.next_node
-    hare = hare.next_node.next_node
-    if hare is tortoise:
-        print(f"Cycle found at {hare.data}")
-        break
+
+def hash_table(head):
+    my_address_hash = set()
+    current = head
+    while current is not None:
+        if id(current) not in my_address_hash:
+            my_address_hash.add(id(current))
+            current = current.next_node
+        else:
+            print(f"Cycle found at {current.data}")
+            break
 
 
+tortoise_hare(a)
+hash_table(a)
