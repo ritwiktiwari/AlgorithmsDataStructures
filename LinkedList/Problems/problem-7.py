2ebc0e9dd690b4053bb8d3b26cfe2fed56c69ff6
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2021-01-13 10:14:34 +0530

    multiple methods find merge point of linked lists

--- LinkedList/Problems/problem-7.py
+++ LinkedList/Problems/problem-7.py
@@ -33,20 +33,26 @@ def find_by_hash(head1, head2):
                 address.add(id(head1))
                 head1 = head1.next_node
             else:
-                print("Merge point is at =", id(head1))
-                print("Data =", head1.data)
+                print("Hash Method: Merge point is at =", id(head1))
+                # print("Data =", head1.data)
                 break
         if head2 is not None:
             if id(head2) not in address:
                 address.add(id(head2))
                 head2 = head2.next_node
             else:
-                print("Merge point is at =", id(head2))
-                print("Data =", head2.data)
+                print("Hash Method: Merge point is at =", id(head2))
+                # print("Data =", head2.data)
                 break
 
 
 def find_by_stack(head1, head2):
+    """
+    Time complexity O(m+n)
+    :param head1:
+    :param head2:
+    :return:
+    """
     temp = None
 
     stack1 = []
@@ -65,9 +71,61 @@ def find_by_stack(head1, head2):
             temp = stack1.pop()
             stack2.pop()
         else:
-            print("Merge point is at =", temp)
+            print("Stack Method: Merge point is at =", temp)
             break
+
+
+def find_by_search_and_sort(head1, head2):
+    address = []
+    while head1 is not None:
+        address.append(id(head1))
+        head1 = head1.next_node
+    address = sorted(address)  # <- sort in log n time
+    while head2 is not None:
+        if id(head2) in address:  # <- search (binary search)
+            print("Search and Sort: Merge point is at =", id(head2))
+            break
+        else:
+            head2 = head2.next_node
+
+
+def find_by_difference_of_node_counts(head1, head2):
+    length1 = 0
+    length2 = 0
+
+    current1 = head1
+    current2 = head2
+
+    while head1 is not None:
+        length1 += 1
+        head1 = head1.next_node
+
+    while head2 is not None:
+        length2 += 1
+        head2 = head2.next_node
+
+    if length1 >= length2:
+        difference = length1 - length2
+        flag = 1
+    else:
+        difference = length2 - length1
+        flag = 2
+
+    for _ in range(difference):
+        if flag == 1:
+            current1 = current1.next_node
+        else:
+            current2 = current2.next_node
+
+    while current1 is not None or current2 is not None:
+        if id(current1) == id(current2):
+            print("Difference Method: Merge point is at =", id(current1))
+            break
+        current1 = current1.next_node
+        current2 = current2.next_node
 
 
 find_by_hash(a1, a2)
 find_by_stack(a1, a2)
+find_by_search_and_sort(a1, a2)
+find_by_difference_of_node_counts(a1, a2)
