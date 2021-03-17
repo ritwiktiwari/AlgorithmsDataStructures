9f4f9817c88024b31b6d080f8062d469c046cd7d
Ritwik Tiwari <ritwiktiwari@hotmail.com>
2021-03-17 18:08:00 +0530

    code clean up

--- Recursion/problem-14.py
+++ Recursion/problem-14.py
@@ -3,15 +3,18 @@ Generate balanced parenthesis
 """
 
 
-def generate_balanced_parenthesis(opening: int, closing: int, string: str):
-    if opening == 0 and closing == 0:
-        print(string)
-    if opening != 0:
-        output1 = string + "("
-        generate_balanced_parenthesis(opening - 1, closing, output1)
-    if closing > opening:
-        output2 = string + ")"
-        generate_balanced_parenthesis(opening, closing - 1, output2)
+def generate_balanced_parenthesis(n: int):
+    def inner(opening: int, closing: int, string: str):
+        if opening == 0 and closing == 0:
+            print(string)
+        if opening != 0:
+            output1 = string + "("
+            inner(opening - 1, closing, output1)
+        if closing > opening:
+            output2 = string + ")"
+            inner(opening, closing - 1, output2)
+
+    inner(n, n, "")
 
 
-generate_balanced_parenthesis(3, 3, "")
+generate_balanced_parenthesis(3)
