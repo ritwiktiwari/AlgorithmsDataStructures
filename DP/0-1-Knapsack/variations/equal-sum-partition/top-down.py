1403f6fe686c0e8e603d522761661ae9e6429be3
Ritwik Tiwari <ritwiktiwari@hotmail.com>
2021-03-23 04:20:29 +0530

    equal sum partition

---
+++ DP/0-1-Knapsack/variations/equal-sum-partition/top-down.py
@@ -0,0 +1,25 @@
+def equal_sum_partition(arr):
+    n = len(arr)
+    s = sum(arr) // 2
+
+    if sum(arr) % 2 != 0:
+        return False
+    else:
+        t = [[False for _ in range(s + 1)] for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            t[i][0] = True
+        for i in range(1, s + 1):
+            t[0][i] = False
+
+        for i in range(n + 1):
+            for j in range(s + 1):
+                if arr[i - 1] <= j:
+                    t[i][j] = t[i - 1][j - arr[i - 1]] or t[i - 1][j]
+                else:
+                    t[i][j] = t[i - 1][j]
+        return t[n][s]
+
+
+my_arr = [1, 5, 11, 5]
+print(equal_sum_partition(my_arr))
