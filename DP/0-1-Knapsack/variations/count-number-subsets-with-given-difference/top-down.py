236e3407ebc5d454c10c2989c2f4d1d4da1851a1
Ritwik Tiwari <ritwiktiwari@hotmail.com>
2021-03-23 07:02:42 +0530

    fixes

--- DP/0-1-Knapsack/variations/count-number-subsets-with-given-difference/top-down.py
+++ DP/0-1-Knapsack/variations/count-number-subsets-with-given-difference/top-down.py
@@ -9,7 +9,7 @@ def solve(arr, d):
 
     for i in range(1, n + 1):
         for j in range(1, s + 1):
-            if arr[n - 1] <= s:
+            if arr[i - 1] <= j:
                 t[i][j] = t[i - 1][j - arr[i - 1]] + t[i - 1][j]
             else:
                 t[i][j] = t[i - 1][j]
